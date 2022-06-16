2a5b4d08d331028f9ec2bcd1e1635b31f43933e8
AsadullohRuziev <asadullohruziev@gmail.com>
2022-06-16 16:25:32 +0500

    all

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
+use App\Http\Controllers\BlogPostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// The route we have created to show all blog posts.
+Route::get('/blog', [BlogPostController::class, 'index']);
+
+
+//show
+Route::get('/blog/{blogPost?}', [BlogPostController::class, 'show']);
+
+
+Route::get('/blog/create/post', [BlogPostController::class, 'create']); //shows create post form
+Route::post('/blog/create/post', [BlogPostController::class, 'store']); //saves the created post to the databse
+Route::get('/blog/{blogPost}/edit', [BlogPostController::class, 'edit']); //shows edit post form
+Route::put('/blog/{blogPost}/edit', [BlogPostController::class, 'update']); //commits edited post to the database
+Route::delete('/blog/{blogPost}', [BlogPostController::class, 'destroy']); //deletes post from the database
+
+
+
+
+
+
+
+
+
